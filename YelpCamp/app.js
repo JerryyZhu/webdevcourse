c12218326dbc4ce5e125a95fc0c9bf0cf1b0e1a4
Jerry Zhu <jerryzhu96@hotmail.com>
2018-11-24 22:17:10 +1100

    styled campgrounds page

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 
+var campgrounds = [
+    {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f3c17dafe5bcbc_340.jpg"},
+    {name: "Granite Hill", image: "https://farm3.staticflickr.com/2929/14442301811_04f2a7f7a2.jpg"},
+    {name: "Stony Peak", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f3c17dafe5bcbc_340.jpg"}        
+]
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 app.get("/", function(req,res){
@@ -8,14 +16,24 @@ app.get("/", function(req,res){
 })
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name: "Salmon Creek", image: "https://pixabay.com/get/e837b1072af4003ed1584d05fb1d4e97e07ee3d21cac104491f3c17dafe5bcbc_340.jpg"},
-        {name: "Granite Hill", image: "https://farm3.staticflickr.com/2929/14442301811_04f2a7f7a2.jpg"},
-        {name: "Stony Peak", image: "https://pixabay.com/get/e834b5062cf4033ed1584d05fb1d4e97e07ee3d21cac104491f3c17dafe5bcbc_340.jpg"}        
-    ]
     res.render("campgrounds", {campgrounds: campgrounds});
 })
 
+app.post("/campgrounds", function(req, res){
+    // get data from form and add to campgrounds array
+    // redirect to campgrounds page
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name :name, image:image}
+    campgrounds.push(newCampground);
+    res.send("Post Campgrounds request");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new.ejs");
+});
+
+
 app.listen(3000, function(){
     console.log("The YelpCamp Server has started.");
-})
+});
