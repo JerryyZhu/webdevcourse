5195add8dfedf6606e8cb619cc03d2764c26897f
Jerry Zhu <jerryzhu96@hotmail.com>
2018-12-03 18:24:21 +1100

    Auth: authentication implemented

--- Authentication/app.js
+++ Authentication/app.js
@@ -10,8 +10,25 @@ var express = require("express"),
 mongoose.connect("mongodb://localhost/auth_demo_app");
 
 var app = express();
-
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+// Used to encode and decode the sessions information
+app.use(require("express-session")({
+    secret: "thisisasecreykeyithink",
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Required for passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Responsible for reading the session (decoding), and 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Routes
 
 app.get("/", function(req, res){
     res.render("home");
@@ -19,6 +36,30 @@ app.get("/", function(req, res){
 
 app.get("/secret", function(req, res){
     res.render("secret");
+});
+
+// Auth routes
+app.get("/register", function(req, res){
+    res.render("register");
+});
+
+app.post("/register", function(req, res){
+    req.body.username 
+    req.body.password
+    // Don't save the password in the database, not a good idea
+    // Hashed password is better
+    User.register(new User({username: req.body.username}), req.body.passowrd, function(err, user){
+        if(err){
+            console.log(err);
+            res.render('register');
+        }
+        // use the local strategy
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("secret");
+        });
+    });
+
+    res.send("Register Post Route");
 });
 
 app.listen(3000, function(){
